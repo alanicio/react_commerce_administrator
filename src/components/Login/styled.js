355a4b2f98d202d80a639e36120943ab4b7a55b6
Alan <alanicio98@outlook.com>
2020-12-24 20:46:08 -0600

    testing colors added

--- src/components/Login/styled.js
+++ src/components/Login/styled.js
@@ -1,8 +1,7 @@
 import styled from "@emotion/styled";
 
-
 export const Background = styled.div`
-  background-color: red;
+  background-color: #d8f1f1;
   width: 100%;
   height: 100vh;
   display: flex;
@@ -13,6 +12,10 @@ export const Card = styled.div`
   padding: 2rem 2rem;
   margin: auto;
   border-radius: 12px;
+
+  -webkit-box-shadow: 0px 0px 5px 1px rgba(255,255,255,1);
+-moz-box-shadow: 0px 0px 5px 1px rgba(255,255,255,1);
+box-shadow: 0px 0px 5px 1px rgba(255,255,255,1);
 `;
 
 export const FormRow = styled.form`
@@ -20,6 +23,8 @@ export const FormRow = styled.form`
   padding-bottom: 1rem;
 
   label {
-    display: ${ ({type}) => type==="text" ? "block" : null };
+    display: ${ ({ type }) => (type === "text" ? "block" : null) };
+    padding-top: 2rem;
+    padding-bottom: ${ ({ type }) => (type=== "text" ? "1rem" : null) };
   }
 `;
